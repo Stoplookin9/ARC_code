5549ee8743f754ca2f14c820cf24f9ae795fd38f
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2022-11-09 15:05:00 -0500

    Create util.py

---
+++ src/util.py
@@ -0,0 +1,10 @@
+# ------------------------------
+# ARC Drone Competition code
+# Main programmer: Stanley Chen
+# 
+# See ARC_code/README.md for full information
+# ------------------------------
+# Utilities for ARC_code
+# Import built in modules
+import math
+import random
