58a02e2e1162cf5c87f251d9c95a4be6ba217a30
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2022-11-10 20:41:40 -0500

    Moving things to other files

--- src/Main.py
+++ src/Main.py
@@ -12,23 +12,3 @@ import time
 from mavsdk import System
 # Create a System() object called drone
 drone = System()
-# Prepare the drone
-def init_drone(): 
-  # The definition drone.action.arm() commands the drone to spin all motors slowly
-  # For safety purposes, we will delay arming by 10 seconds. 
-  print("Drone is arming in 10 seconds, please clear area of all objects and personal!")
-  time.sleep(5)
-  print("Drone is arming in 5 seconds!")
-  time.sleep(5)
-  print("Drone arming!")
-  # Arm the drone
-  await drone.action.arm()
-  
-  
-# Commands for test flight
-# Note: Drone arming is done seperately in the definition init_drone()
-def test_flight:
-  time.sleep(3)
-  await drone.action.disarm()
-
-init_drone()
