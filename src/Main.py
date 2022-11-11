747505631794873ba562951e9fa9292be92ae24c
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2022-11-10 21:38:13 -0500

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -8,10 +8,15 @@
 import math
 import random
 import time
+import json
 # Import MAVSDK library, this library will be actually
 # controlling the drone (such as taking off or setting max speed)
 from mavsdk import System
 # Create a System() object called drone
 drone = System()
 # Load config settings
-f = open("", "r")
+f = open("config.json", "r")
+# Create a dictionary and parse json from the file into it
+config = json.loads(f.read())
+
+# config["max_speed"]
