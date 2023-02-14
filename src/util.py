d2e6a4ca99ccdae4cf4c5b10efc64cf65c25d11c
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2023-02-14 16:12:53 -0500

    Update util.py

--- src/util.py
+++ src/util.py
@@ -8,3 +8,17 @@
 # Import built in modules
 import math
 import random
+# Basic math formulas (self-explanatory)
+def getDistance(x1, y1, x2, y2):
+  return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+def getDistance3(x1, y1, z1, x2, y2, z2): 
+  return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+def getArcLength(radius, theta): 
+  return radius * theta;
+def degToRad(deg): 
+  return deg * math.pi / 180
+def radToDeg(rad):
+  return rad * 180 / math.pi
+# More complex formulas
+# Trajectory calculation
+def calculateTrajectory(x):
