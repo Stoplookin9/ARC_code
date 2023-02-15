ef40844c1fdfbf2d776748bee32335fd2348b514
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2023-02-15 13:55:05 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
+# ------------------------------
+# ARC Drone Competition code
+# Main programmer: Stanley Chen
+# 
+# See ARC_code/README.md for full information
 import json
-#get config values
+#get config values from config.json
 configReader = open("config.json")
 config = json.loads(configReader.read())
