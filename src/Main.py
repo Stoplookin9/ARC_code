317a0598ffe9cab791ba68e150546edc69f758f1
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2023-04-16 16:32:24 -0400

    Made some changes, should work

--- src/Main.py
+++ src/Main.py
@@ -15,4 +15,21 @@ import asyncio
 from mavsdk import System
 # Create a System() object called drone
 drone = System()
+# The main function
+# This should work, as I copied most of it from the official Github repo
+async def run():
+  # Connect to drone
+  await drone.connect(system_address="udp://:14540")
+  async for state in drone.core.connection_state():
+    if state.is_connected:
+      print(f"Drone discovered with UUID: {state.uuid}")
+      break
+  # 2 second safety margin
+  await asyncio.sleep(2)
+  # Arm the drone, then let it automatically shut down
+  await drone.action.arm()
 
+# Run it  
+if __name__ == "__main__":
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(run())
