65aae4e0c22db32681471c5014560b85c9f4be35
Stoplookin9 <83974006+Stoplookin9@users.noreply.github.com>
2023-05-05 19:27:49 -0400

    Delete main.py

--- waypoint/main.py
+++
@@ -1,6 +0,0 @@
-# ------------------------------
-# ARC Drone Competition code
-# Main programmer: Stanley Chen
-#
-# See ARC_code/waypoint/README.md for full information
-# ------------------------------
